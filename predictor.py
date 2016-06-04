d89813c7ad5132f4e3181df947351bcb157cfce6
Vladimir Sidorov <sidorvm@gmail.com>
2016-06-04 12:53:25 +0900

    Some bunch of code

--- predictor.py
+++ predictor.py
@@ -1,2 +1,46 @@
 import pandas
+import random
 
+# Pick 10000 users
+unique_users = train.user_id.unique()
+
+sel_user_ids = [unique_users[i] for i in sorted(random.sample(range(len(unique_users)), 10000)) ]
+sel_train = train[train.user_id.isin(sel_user_ids)]
+
+# Pick new training and testing sets
+t1 = sel_train[((sel_train.year == 2013) | ((sel_train.year == 2014) & (sel_train.month < 8)))]
+t2 = sel_train[((sel_train.year == 2014) & (sel_train.month >= 8))]
+
+# Remove click events
+t2 = t2[t2.is_booking == True]
+
+
+df = calc_fast_features(t1)
+
+def calc_fast_features(df):
+    df["date_time"] = pandas.to_datetime(df["date_time"])
+    df["srch_ci"] = pandas.to_datetime(df["srch_ci"], format='%Y-%m-%d', errors="coerce")
+    df["srch_co"] = pandas.to_datetime(df["srch_co"], format='%Y-%m-%d', errors="coerce")
+    
+    props = {}
+    for prop in ["month", "day", "hour", "minute", "dayofweek", "quarter"]:
+        props[prop] = getattr(df["date_time"].dt, prop)
+    
+    carryover = [p for p in df.columns if p not in ["date_time", "srch_ci", "srch_co"]]
+    for prop in carryover:
+        props[prop] = df[prop]
+    
+    date_props = ["month", "day", "dayofweek", "quarter"]
+    for prop in date_props:
+        props["ci_{0}".format(prop)] = getattr(df["srch_ci"].dt, prop)
+        props["co_{0}".format(prop)] = getattr(df["srch_co"].dt, prop)
+    props["stay_span"] = (df["srch_co"] - df["srch_ci"]).astype('timedelta64[h]')
+        
+    ret = pandas.DataFrame(props)
+    
+    ret = ret.join(dest_small, on="srch_destination_id", how='left', rsuffix="dest")
+    ret = ret.drop("srch_destination_iddest", axis=1)
+    return ret
+
+
+def random
