74f579a50afdf3fe4553c6ecddd91b936f88d7a7
Ugo Bataillard <ugo@bataillard.me>
2016-06-04 12:47:41 +0900

    Added beginning of tutorial to load the data

--- predictor.py
+++ predictor.py
@@ -1,1 +1,23 @@
-import pandas
+import pandas as pd
+import random
+import ml_metrics as metrics
+
+destinations = pd.read_csv("destinations.csv")
+test = pd.read_csv("test.csv")
+train = pd.read_csv("train.csv")
+
+train["date_time"] = pd.to_datetime(train["date_time"])
+train["year"] = train["date_time"].dt.year
+train["month"] = train["date_time"].dt.month
+
+unique_users = train.user_id.unique()
+
+sel_user_ids = [unique_users[i] for i in sorted(random.sample(range(len(unique_users)), 10000)) ]
+sel_train = train[train.user_id.isin(sel_user_ids)]
+
+t1 = sel_train[((sel_train.year == 2013) | ((sel_train.year == 2014) & (sel_train.month < 8)))]
+t2 = sel_train[((sel_train.year == 2014) & (sel_train.month >= 8))]
+
+t2 = t2[t2.is_booking == True]
+
+
